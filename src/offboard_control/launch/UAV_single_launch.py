0453b7b546a219d554f4a50c313524f363fe9480
James Wright <jw.wright@connect.qut.edu.au>
2023-10-18 23:27:07 +1000

    Updated functionality

---
+++ src/offboard_control/launch/UAV_single_launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='offboard_control',
+            namespace='UAV1',
+            executable='UAV_control',
+            name='sim',
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                {'UAV_ID': '1'}
+            ]
+        )
+    ])
