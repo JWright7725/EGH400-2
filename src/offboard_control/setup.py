2b209ef95184d7e7695d50e29a7790ce0a197235
James Wright <jw.wright@connect.qut.edu.au>
2023-10-18 20:51:57 +1000

    Created Launch file for the UAV Control Package

--- src/offboard_control/setup.py
+++ src/offboard_control/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'offboard_control'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'UAV_control = offboard_control.UAV_control:main'
+            'UAV_control = offboard_control.UAV_control:main',
         ],
     },
 )
