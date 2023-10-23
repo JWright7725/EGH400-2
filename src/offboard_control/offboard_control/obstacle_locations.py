15ad1ee7e9fbb656ff76c3f4e954296f3a4dd5e4
James Wright <jw.wright@connect.qut.edu.au>
2023-10-24 00:15:05 +1000

    Updated Obstacle Locations

--- src/offboard_control/offboard_control/obstacle_locations.py
+++ src/offboard_control/offboard_control/obstacle_locations.py
@@ -1,18 +1,18 @@
 def obstacle_locations():
     """Define an array of obstacle locations within the nodal representation of the world frame"""
     # Obstacle 1
-    obstacle1_x_min = 10
-    obstacle1_x_max = 14
-    obstacle1_y_min = 16
-    obstacle1_y_max = 18
+    obstacle1_x_min = 7
+    obstacle1_x_max = 17
+    obstacle1_y_min = 18
+    obstacle1_y_max = 22
     obstacle1_z_min = 0
     obstacle1_z_max = 10
 
     # Obstacle 2
-    obstacle2_x_min = 16
-    obstacle2_x_max = 18
-    obstacle2_y_min = 16
-    obstacle2_y_max = 18
+    obstacle2_x_min = 24
+    obstacle2_x_max = 28
+    obstacle2_y_min = 14
+    obstacle2_y_max = 24
     obstacle2_z_min = 0
     obstacle2_z_max = 10
 
